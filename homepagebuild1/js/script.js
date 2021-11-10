0a219bfc2ed2d7884bf8cf6d8c49967a9c7d500c
yamadashota0612 <87640033+yamadashota0612@users.noreply.github.com>
2021-11-10 14:13:45 +0900

    Add files via upload

---
+++ homepagebuild1/js/script.js
@@ -0,0 +1,38 @@
+// スティッキーヘッダー
+$(function(){
+  var $win = $(window),
+    $fv = $(`.fv`),
+    $header = $(`.header`),
+    fvHeight = $fv.outerHeight();
+    fixedClass = `fixed`;
+  
+  $win.on(`load scroll`,function(){
+    var value = $(this).scrollTop();
+    if($win.width() > 768){
+      if( value > fvHeight){
+        $header.addClass(fixedClass);
+      }else{
+        $header.removeClass(fixedClass);
+      }
+    }
+    });
+});
+
+
+// スライダー
+
+$(`.slider`).slick({
+  autoplay: true,
+  autoplaySpeed: 2000,
+  speed: 1000,
+  fade: true,
+  cssEase: `linear`
+});
+
+// ハンバーガーメニュー
+
+$(`.burger-btn`).on(`click`,function(){
+  $(`.header-nav`).fadeToggle(300);
+  $(this).toggleClass(`cross`);
+  $(`body`).toggleClass(`noscroll`);
+});
